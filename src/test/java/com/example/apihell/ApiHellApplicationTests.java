02fd356fda03b7bb321c13d11a98ec8664d1f5ea
maxkrivenya <maxkrivenya@gmail.com>
2024-02-22 22:46:55 +0300

    test update

--- src/test/java/com/example/apihell/ApiHellApplicationTests.java
+++ src/test/java/com/example/apihell/ApiHellApplicationTests.java
@@ -1,14 +1,19 @@
 package com.example.apihell;
-
+import com.example.apihell.model.Student;
+import com.example.apihell.repository.Repository;
+import com.example.apihell.controller.Controller;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class ApiHellApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println("hello world");
+        Controller controller = null;
+        ResponseEntity<Student> student = controller.getStudentById("25050071");
     }
 
 }
