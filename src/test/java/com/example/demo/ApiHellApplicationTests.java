357159b1133e9e4f919b72063c88c2aca0c61c82
maxkrivenya <maxkrivenya@gmail.com>
2024-02-27 03:34:28 -0500

    why

--- src/test/java/com/example/demo/ApiHellApplicationTests.java
+++ src/test/java/com/example/demo/ApiHellApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.apihell.ApiHellApplication;
+import jakarta.xml.ws.http.HTTPException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
@@ -9,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootConfiguration
 class ApiHellApplicationTests {
     @Test
-    void contextLoads() {
+    void contextLoads() throws HTTPException {
         SpringApplication.run(ApiHellApplication.class);
     }
 }
