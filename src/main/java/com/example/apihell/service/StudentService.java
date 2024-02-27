0eb72bc7ae98d6644a1a641f55098b75b7100dc1
maxkrivenya <maxkrivenya@gmail.com>
2024-02-27 04:51:48 -0500

    service

--- src/main/java/com/example/apihell/service/StudentService.java
+++ src/main/java/com/example/apihell/service/StudentService.java
@@ -5,7 +5,6 @@ import com.example.apihell.repository.MarksRep;
 import com.example.apihell.repository.Repository;
 import com.example.apihell.repository.SemRep;
 import jakarta.annotation.Nullable;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
